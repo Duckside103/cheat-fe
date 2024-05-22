a4496203ade9d3ec4c2b077a389f3297321587ab
SoftDrink <anhvu.dev.103@gmail.com>
2024-05-22 10:22:47 +0700

    test 2

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,37 +1,36 @@
-// import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 // import useDetail from "../hooks/useDetail";
-// import { useState } from "react";
-// import axios from "axios";
-
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Detail = () => {
   const navigate = useNavigate();
   console.log(">> Check | navigate:", navigate);
-  // let { id } = useParams();
-  // const [_id, number] = id.split("ahihi");
+  let { id } = useParams();
+  // eslint-disable-next-line no-unused-vars
+  const [_id, number] = id.split("ahihi");
 
-  // const [value, setValue] = useState("");
-  // const [numberVal, setNumberVal] = useState(number);
+  const [value, setValue] = useState("");
+  const [numberVal, setNumberVal] = useState(number);
 
   // const detail = useDetail(_id);
 
-  // const handleChange = (e) => {
-  //   setValue(e.target.value);
-  // };
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-  // const handleChangeNumber = (e) => {
-  //   setNumberVal(e.target.value);
-  // };
+  const handleChangeNumber = (e) => {
+    setNumberVal(e.target.value);
+  };
 
-  // const handleSubmit = async () => {
-  //   await axios.post("https://smart-guy.vercel.app/answer", {
-  //     number,
-  //     answer: value,
-  //   });
+  const handleSubmit = async () => {
+    await axios.post("https://smart-guy.vercel.app/answer", {
+      number,
+      answer: value,
+    });
 
-  //   navigate("/");
-  // };
+    navigate("/");
+  };
 
   return (
     <div
@@ -41,14 +40,14 @@ const Detail = () => {
         flexDirection: "column",
       }}
     >
-      {/* <div>
+      <div>
         <input type="text" value={numberVal} onChange={handleChangeNumber} />
         <input type="text" autoFocus value={value} onChange={handleChange} />
         <button onClick={handleSubmit} disabled={!value}>
           Submit
         </button>
       </div>
-      <img width={1400} src={detail.html} /> */}
+      {/* <img width={1400} src={detail.html} /> */}
     </div>
   );
 };
